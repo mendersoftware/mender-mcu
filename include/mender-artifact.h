40c2082e5b1e86aede77f02b9cb51d44e9b58165
Lars Erik Wik <lars.erik.wik@northern.tech>
2024-10-01 09:42:56 +0200

    docs: fixed misleading doc string for mender_artifact_get_ctx
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- include/mender-artifact.h
+++ include/mender-artifact.h
@@ -94,7 +94,7 @@ mender_err_t mender_artifact_get_device_type(mender_artifact_ctx_t *ctx, const c
 mender_artifact_ctx_t *mender_artifact_create_ctx(void);
 
 /**
- * @brief Function used to create a new artifact context
+ * @brief Function used to get the artifact context
  * @return MENDER_OK if the function succeeds, error code otherwise
  */
 mender_err_t mender_artifact_get_ctx(mender_artifact_ctx_t **ctx);

51ad563d924d13856402dd1875f08777d66ba6af
Daniel Skinstad Drabitzius <daniel.drabitzius@northern.tech>
2024-10-30 09:15:01 +0100

    chore: remove compiler warning for `vasprintf`
    Ticket: None
    
    Signed-off-by: Daniel Skinstad Drabitzius <daniel.drabitzius@northern.tech>

--- platform/net/zephyr/src/mender-net.c
+++ platform/net/zephyr/src/mender-net.c
@@ -16,6 +16,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#define _GNU_SOURCE //vasprintf
+#include <stdio.h>  //vasprintf
 
 #include <errno.h>
 #include <zephyr/net/socket.h>

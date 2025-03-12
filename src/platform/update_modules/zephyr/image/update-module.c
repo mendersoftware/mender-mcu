ea54f37c701cedec38c846acf8c1d79b9a3b2798
Vratislav Podzimek <vratislav.podzimek+auto-signed@northern.tech>
2025-03-12 14:31:34 +0100

    chore: Move the zephyr-image update module under `src/platform`
    After the headers and sources reorganization there's no need to
    keep it in the `src/core` directory where it quite obviously
    doesn't belong.
    
    Ticket: MEN-7554
    Changelog: none
    Signed-off-by: Vratislav Podzimek <vratislav.podzimek+auto-signed@northern.tech>

--- src/platform/update_modules/zephyr/image/update-module.c
+++ src/platform/update_modules/zephyr/image/update-module.c
@@ -1,5 +1,5 @@
 /**
- * @file      zephyr-image-update-module.c
+ * @file      update-module.c
  * @brief     The basic Zephyr update module based on MCUboot
  *
  * Copyright joelguittet and mender-mcu-client contributors

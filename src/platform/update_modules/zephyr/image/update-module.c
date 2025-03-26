322e35d96edd95219ac50ac94ca220ad013ade6e
Daniel Skinstad Drabitzius <daniel.drabitzius@northern.tech>
2025-03-26 13:08:54 +0100

    fix: fail rollback if we're already in a confirmed image
    Ticket: MEN-8215
    Changelog: Title
    
    Signed-off-by: Daniel Skinstad Drabitzius <daniel.drabitzius@northern.tech>

--- src/platform/update_modules/zephyr/image/update-module.c
+++ src/platform/update_modules/zephyr/image/update-module.c
@@ -189,6 +189,11 @@ static mender_err_t mender_zephyr_image_abort_deployment(mender_update_state_t s
 static mender_err_t mender_zephyr_image_reboot_callback(mender_update_state_t state, mender_update_state_data_t callback_data);
 
 /**
+ * @brief Rollback callback
+ */
+static mender_err_t mender_zephyr_image_rollback_callback(mender_update_state_t state, mender_update_state_data_t callback_data);
+
+/**
  * @brief New image verification callback
  */
 static mender_err_t mender_zephyr_image_verify_reboot_callback(mender_update_state_t state, mender_update_state_data_t callback_data);
@@ -215,6 +220,7 @@ mender_zephyr_image_register_update_module(void) {
     zephyr_image_umod->callbacks[MENDER_UPDATE_STATE_COMMIT]        = &mender_zephyr_image_confirm_image;
     /* no need for a rollback callback because a reboot without image confirmation is a rollback */
     zephyr_image_umod->callbacks[MENDER_UPDATE_STATE_FAILURE]         = &mender_zephyr_image_abort_deployment;
+    zephyr_image_umod->callbacks[MENDER_UPDATE_STATE_ROLLBACK]        = &mender_zephyr_image_rollback_callback;
     zephyr_image_umod->callbacks[MENDER_UPDATE_STATE_ROLLBACK_REBOOT] = &mender_zephyr_image_reboot_callback;
     zephyr_image_umod->artifact_type                                  = "zephyr-image";
     zephyr_image_umod->requires_reboot                                = true;
@@ -325,6 +331,17 @@ mender_zephyr_image_reboot_callback(MENDER_NDEBUG_UNUSED mender_update_state_t s
         mender_log_error("Reboot requested, but no reboot support");
         return MENDER_FAIL;
     }
+}
+
+static mender_err_t
+mender_zephyr_image_rollback_callback(mender_update_state_t state, mender_update_state_data_t callback_data) {
+    assert(MENDER_UPDATE_STATE_ROLLBACK == state);
+    // Don't attempt to rollback if we're in a confirmed image - e.g. if an aborted deployment is detected in `MENDER_UPDATE_STATE_REBOOT`
+    if (mender_flash_is_image_confirmed()) {
+        mender_log_debug("Current image is confirmed, nothing to rollback");
+        return MENDER_FAIL;
+    }
+    return MENDER_OK;
 }
 
 static mender_err_t

11b69d049eba337891c121fb0a89fe10f54e8b53
Vratislav Podzimek <vratislav.podzimek+auto-signed@northern.tech>
2026-09-07 13:43:48 +0200

    feat: Integrate mender-artifact into ESP-IDF builds
    Making it easier for developers to deploy updates using Mender.
    
    Many parts of the logic can be shared between Zephyr and ESP-IDF
    with a bit of refactoring.
    
    Ticket: None
    Changelog: None
    Signed-off-by: Vratislav Podzimek <vratislav.podzimek+auto-signed@northern.tech>

--- target/zephyr/mender-artifact.cmake
+++ target/zephyr/mender-artifact.cmake
@@ -1,5 +1,5 @@
 # @file      mender-artifact.cmake
-# @brief     CMake code to generate the Mender Artifact
+# @brief     Zephyr-specific CMake code to generate Mender Artifacts
 #
 # Copyright Northern.tech AS
 #
@@ -15,68 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Check for the tool and the Artifact Name. The rest of the parameters have defaults
-find_program(mender_artifact_found mender-artifact)
-if(NOT mender_artifact_found)
-    message(FATAL_ERROR "mender-artifact not found in PATH. Visit https://docs.mender.io/downloads#mender-artifact to download the tool or disable Artifact generation with MENDER_ARTIFACT_GENERATE")
-endif()
+include(${CMAKE_CURRENT_LIST_DIR}/../../cmake/mender-artifact.cmake)
 
-# Fail if version mender-artifact version doesn't have the Artifact size functionality
-if (CONFIG_MENDER_ARTIFACT_SIZE_LIMITS)
-    execute_process(COMMAND mender-artifact write module-image --help OUTPUT_VARIABLE HELP_OUTPUT OUTPUT_STRIP_TRAILING_WHITESPACE)
-    string(FIND "${HELP_OUTPUT}" "--warn-artifact-size" FOUND_ARTIFACT_SIZE)
-    if (FOUND_ARTIFACT_SIZE LESS 0)
-        message(FATAL_ERROR "Setting Artifact size limits require mender-artifact >= 4.2.0")
-    endif()
-endif()
-
-# Print a warning on empty Artifact name
-if(CONFIG_MENDER_ARTIFACT_NAME STREQUAL "")
-    message(WARNING "MENDER_ARTIFACT_NAME cannot be empty; Artifact generation will fail. Set the variable in your build or alternatively disable the feature with CONFIG_MENDER_ARTIFACT_GENERATE=n")
-endif()
-
-# Assemble the mender-artifact arguments
-set(mender_artifact_cmd mender-artifact write module-image)
-# No compression
-set(mender_artifact_cmd ${mender_artifact_cmd} --compression none)
-# Artifact name
-set(mender_artifact_cmd ${mender_artifact_cmd} --artifact-name '${CONFIG_MENDER_ARTIFACT_NAME}')
-# Update Module
-set(mender_artifact_cmd ${mender_artifact_cmd} --type ${CONFIG_MENDER_ARTIFACT_TYPE})
-# Device type
-string(REPLACE " " ";" device_type_list ${CONFIG_MENDER_DEVICE_TYPES_COMPATIBLE})
-foreach(device_type ${device_type_list})
-    set(mender_artifact_cmd ${mender_artifact_cmd} --compatible-types ${device_type})
-endforeach()
-# Artifact provides
-if(NOT CONFIG_MENDER_ARTIFACT_PROVIDES STREQUAL "")
-    string(REPLACE " " ";" provides_list ${CONFIG_MENDER_ARTIFACT_PROVIDES})
-    foreach(provides ${provides_list})
-        set(mender_artifact_cmd ${mender_artifact_cmd} --provides ${provides})
-    endforeach()
-endif()
-# Artifact depends
-if(NOT CONFIG_MENDER_ARTIFACT_DEPENDS STREQUAL "")
-    string(REPLACE " " ";" depends_list ${CONFIG_MENDER_ARTIFACT_DEPENDS})
-    foreach(depends ${depends_list})
-        set(mender_artifact_cmd ${mender_artifact_cmd} --depends ${depends})
-    endforeach()
-endif()
-# Prefix for the default provides
-set(mender_artifact_cmd ${mender_artifact_cmd} --software-filesystem ${CONFIG_MENDER_ARTIFACT_SOFTWARE_FILESYSTEM})
-set(mender_artifact_cmd ${mender_artifact_cmd} --software-name ${CONFIG_MENDER_ARTIFACT_SOFTWARE_NAME})
-# Set fail/warn limits on Artifact size
-if (CONFIG_MENDER_ARTIFACT_WARN_SIZE)
-    set(mender_artifact_cmd ${mender_artifact_cmd} --warn-artifact-size ${CONFIG_MENDER_ARTIFACT_WARN_SIZE})
-endif()
-if (CONFIG_MENDER_ARTIFACT_MAX_SIZE)
-    set(mender_artifact_cmd ${mender_artifact_cmd} --max-artifact-size ${CONFIG_MENDER_ARTIFACT_MAX_SIZE})
-endif()
-# Extra arguments
-if(NOT CONFIG_MENDER_ARTIFACT_EXTRA_ARGS STREQUAL "")
-    separate_arguments(extra_args UNIX_COMMAND ${CONFIG_MENDER_ARTIFACT_EXTRA_ARGS})
-    set(mender_artifact_cmd ${mender_artifact_cmd} ${extra_args})
-endif()
 # Input file
 if(CONFIG_MENDER_ARTIFACT_PAYLOAD_FILE STREQUAL "")
     set(mender_artifact_payload ${ZEPHYR_BINARY_DIR}/${KERNEL_NAME}.signed.bin)
@@ -91,7 +31,6 @@ else()
     set(mender_artifact_output ${CONFIG_MENDER_ARTIFACT_OUTPUT_FILE})
 endif()
 set(mender_artifact_cmd ${mender_artifact_cmd} --output-path ${mender_artifact_output})
-
 
 #### Design note ###
 #
